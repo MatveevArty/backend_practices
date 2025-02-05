e08f7f9ec0b208e430ef063ad9b0c2f708aaef4f
Artyom Matveev <matveev.arty@gmail.com>
2025-02-05 23:04:26 +0700

    backend-2: CORS integrated

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express'); // Импортируем установленный модуль express
 const app = express(); // Объявляем переменную app на основе фреймворка express
 const routes = require('./routes/index');
+const cors = require('cors');
 
 // Задаём использование json-формата для всего приложения
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); // настраиваем специальную кодировку для параметров
+app.use(cors());
 
 // Главный роутер для бэкенд-приложения
 app.use('/api', routes); // используем именно приставку /api, чтобы все обрабатываемые url бэкенд-приложением, а далее шли конкретнные сущности
